5a1b012055a92defc4c3bad7735038d60644de24
Luis Gabriel de M Fernandes <lgabrielmf@gmail.com>
2022-08-01 09:44:30 -0300

    Example of consuming data using Kafka

--- src/main/java/com/gabrielfernandes/estudo/kafka/wikimedia/WikimediaChangesProducer.java
+++ src/main/java/com/gabrielfernandes/estudo/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,39 @@
 package com.gabrielfernandes.estudo.kafka.wikimedia;
 
-public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.0.1.11:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // set high throughput producer settings
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
     }
 }
